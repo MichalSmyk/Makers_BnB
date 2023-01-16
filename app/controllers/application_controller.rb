75f29ad6c112c3b7b3e80849d8dd960a5f5b0fcc
abodian <ajnbodian@gmail.com>
2023-01-16 17:28:41 +0000

    application controller amended to enable sessions and register active record sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,14 @@ require_relative "../../config/environment"
 
 
 class Application < Sinatra::Base
+  enable :sessions
+  register Sinatra::ActiveRecordExtension
   configure :development do
     register Sinatra::Reloader
+  end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
   end
 
   get '/' do
---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+require 'faker'
+
+puts "🌱 Seeding spices..."
+
+# 20.times do
+#   User.create(
+#     username: Faker::Internet.unique.username,
+#     password: Faker::Internet.password,
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#   )
+# end
+
+# 20.times do
+#   Peep.create(
+#     content: Faker::Lorem.sentence(word_count: rand(2..10)).chomp('.'),
+#     time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
+#     user_id: Faker::Number.between(from: 1, to: 20)
+#   )
+# end
+
+# 10.times do
+#   Reply.create(
+#     content: Faker::Lorem.sentence(word_count: rand(2..10)).chomp('.'),
+#     time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
+#     user_id: Faker::Number.between(from: 1, to: 20),
+#     peep_id: Faker::Number.between(from: 1, to: 20)
+#   )
+# end
+
+puts "✅ Done seeding!"
