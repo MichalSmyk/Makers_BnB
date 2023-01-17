cc0081c2766add01aa8bbe818941c9bafff6bd18
MichalSmyk <michal.smyk39@gmail.com>
2023-01-17 14:18:26 +0000

    changed erb space for spaces_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,11 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  get '/space' do 
+  get '/space/:id' do 
     @user = User.new #so you can see user from space page
     @availability = SpaceDate.new #so you can see availability on the space page
     @space = Space.new #so you can see info about the listing
-    erb :space
+    erb :spaces_id
   end 
 
 
