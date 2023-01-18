505cdab62f529571b546f72b2c553bd73a30539e
Michal Smyk <74204358+MichalSmyk@users.noreply.github.com>
2023-01-18 13:44:23 +0000

    Update app_spec.rb

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -137,10 +137,23 @@ describe ApplicationController do
   end
 
   context 'POST /space ' do 
-    it 'sends request to book specific space ' do 
-      response = post('/space/1')
+    it 'sends request to book specific space  if user is logged in' do 
+      response = post('/space/1', stay_date: "20-02-2023", request_time: "19-01-2023", request_approval: "1",
+        space_id: "1", user_id: "1")
 
       expect(response.status).to eq(200)
+      expect(response.body).to include("<title>Booking confirmation</title>")
+
+      booking = Booking.find_by(user_id: "1")
+      booking.destroy
+    end
+
+    it 'returns to sign up page if person requesting booking is not logged in' do 
+       response = post('/space/1', stay_date: "20-02-2023", request_time: "19-01-2023", request_approval: "1",
+        space_id: "1", user_id: "1")
+
+      expect(response.status).to eq (200)
+      expect(response.body).to include('<h1>Sign Up - Create a new MakersBnB Account</h1>')
     end
   end
 end
